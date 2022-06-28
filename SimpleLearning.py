859676364553fdb89a839624a6212d6865b6df01
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-28 13:51:46 +0300

    Creating a training dataset

---
+++ SimpleLearning.py
@@ -0,0 +1,20 @@
+from random import randint
+from sklearn.linear_model import LinearRegression
+
+# Creating a training dataset
+train_set_limit = 1000
+train_set_count = 100
+
+train_input = list()
+train_output = list()
+
+for i in range(train_set_limit):
+	a = randint(0, train_set_limit)
+	b = randint(0, train_set_limit)
+	c = randint(0, train_set_limit)
+	op = a + (2*b) + (3*c)
+	train_input.append([a, b ,c])
+	train_output.append(op)
+
+for i in range(20):
+	print(train_input[i], train_output[i])
