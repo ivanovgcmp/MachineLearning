74932a06061e49c1e9f935c415b820ffd7f23719
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-29 15:13:47 +0300

    Deleting a "unnamed column"

--- DiamondsPredicting.py
+++ DiamondsPredicting.py
@@ -9,3 +9,9 @@ from sklearn.model_selection import train_test_split
 df = pd.read_csv('P1_diamonds.csv')
 
 print(df.head(10).to_string())
+
+# Deleting a "unnamed column"
+df = df.drop(['Unnamed: 0'], axis = 1)
+
+# Creating variables for categories
+
